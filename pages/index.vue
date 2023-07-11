04eab00962230f2dd8a6bd6deb6b35ce49ca3a1a
Daniel Kelly <me@danielkelly.io>
2023-07-11 13:15:41 -0500

    3-vue-js-charts-with-async-data

--- pages/index.vue
+++ pages/index.vue
@@ -19,29 +19,27 @@ ChartJS.register(
   CategoryScale,
   LinearScale
 );
-const chartData = ref<ChartData<"bar">>({
-  labels: [
-    "Spider-Man (2002)",
-    "Spider-Man 2 (2004)",
-    "Spider-Man 3 (2007)",
-    "The Amazing Spider-Man (2012)",
-    "The Amazing Spider-Man 2 (2014)",
-    "Spider-Man: Homecoming (2017)",
-    "Spider-Man: Far From Home (2019)",
-    "Spider-Man: No Way Home (2021)",
-    "Spider-Man: Into the Spider-Verse (2018)",
-    "Spider-Man: Across the Spider-Verse (2023)",
-  ],
-  datasets: [
-    {
-      backgroundColor: ["#c82834", "#244771"],
-      data: [7.3, 7.3, 6.2, 6.9, 6.6, 7.4, 7.5, 8.0, 8.4, 8.9],
-    },
-  ],
+
+type movie = { title: string; rating: number };
+const movies = ref<movie[]>([]);
+fetch("/api.json")
+  .then(async (res) => (movies.value = await res.json()))
+  .catch((error) => alert(error.message));
+const chartData = computed((): ChartData<"bar"> => {
+  return {
+    labels: movies.value.map((movie) => movie.title),
+    datasets: [
+      {
+        backgroundColor: ["#c82834", "#244771"],
+        data: movies.value.map((movie) => movie.rating),
+      },
+    ],
+  };
 });
 </script>
 <template>
   <Bar
+    v-if="movies.length"
     :data="chartData"
     :options="{
       plugins: {
